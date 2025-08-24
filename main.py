0e7524a77c77c2ac4315ccece3105e1a206ef3de
Deepansh-Umar <umardeepansh@gmail.com>
2025-08-24 20:26:59 +0530

    Update main.py
    created appointment class
    created the master class Hospital to manage the other classes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Patient:
     def __init__(self,pid,name,age,illness):
         self.pid=pid
@@ -15,3 +17,21 @@ class Doctor:
         self.spec=spec
     def __str__(self):
         return f"{self.did} - Dr. {self.name} ({self.spec})"
+class Appointment:
+    def __init__(self,pat,doc):
+        self.pat=pat
+        self.doc=doc
+    def __str__(self):
+        return f"{self.pat.name} : Dr. {self.doc.name}"
+
+class Hospital:
+    def __init__(self):
+        self.patients={}
+        self.doctors={}
+        self.appointments=deque()
+    def add_patient(self,pid,name,age,ill):
+        if pid in self.patients:
+            print("ID exists")
+            return
+        self.patients[pid]=Patient(pid,name,age,ill)
+        print("Added the pateint.")
