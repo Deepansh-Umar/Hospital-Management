08903fa8164cda8447121884bc588cec66f129ab
Deepansh-Umar <umardeepansh@gmail.com>
2025-08-24 20:33:13 +0530

    Update main.py
    driver code added for testing

--- main.py
+++ main.py
@@ -49,3 +49,32 @@ class Hospital:
             return
         for a in self.appointments:
             print(a)
+
+
+##driver code
+h=Hospital()
+
+while 1:
+    print("1.Add Patient 2.Add Doctor 3.Book 4.Show 5.Exit")
+    c=input(">> ")
+    if c=='1':
+        pid=input("pid: ")
+        name=input("name: ")
+        age=input("age: ")
+        ill=input("illness: ")
+        h.add_patient(pid,name,age,ill)
+    elif c=='2':
+        did=input("did: ")
+        name=input("name: ")
+        spec=input("spec: ")
+        h.add_doctor(did,name,spec)
+    elif c=='3':
+        pid=input("patient id: ")
+        did=input("doctor id: ")
+        h.book(pid,did)
+    elif c=='4':
+        h.view_apps()
+    elif c=='5':
+        break
+    else:
+        print("???")
