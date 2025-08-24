6fd7ec8443ddcef6425d602d8856c543f3505755
Deepansh-Umar <umardeepansh@gmail.com>
2025-08-24 20:23:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ class Patient:
         self.illness=illness
     def __str__(self):
         return f"{self.pid} - {self.name} ({self.age}) : {self.illness}"
+
+
+class Doctor:
+    def __init__(self,did,name,spec):
+        self.did= did
+        self.name =name
+        self.spec=spec
+    def __str__(self):
+        return f"{self.did} - Dr. {self.name} ({self.spec})"
